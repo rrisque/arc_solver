855bdc8c828008e46e2809e7e728503fc008d5fe
Rob Risque <rob.risque@gmail.com>
2020-04-22 14:11:18 -0400

    randomly select a task, reward=1 if problem solved correctly. reselects a new task after 3 tries

--- arc_gym/envs/arc_env.py
+++ arc_gym/envs/arc_env.py
@@ -1,4 +1,10 @@
+import json
+import os
+import random
+
 import gym
+
+from utils.utils import plot_task
 
 
 class ARCEnv(gym.Env):
@@ -9,18 +15,35 @@ class ARCEnv(gym.Env):
 
     def __init__(self):
         super(ARCEnv, self).__init__()
+        self.training_dir = '../../data/training/'
+        self.tasks = []
+        self._populate_tasks()
+        self.task = random.choice(self.tasks)
+        self.attempts = 0
+
+    def _populate_tasks(self):
+        for filename in os.listdir(self.training_dir):
+            with open(self.training_dir + filename, 'r') as f:
+                self.tasks.append(json.load(f))
 
     def step(self, action):
-        ...
+        reward = 0
+        if action == self.task['test'][0]['output']:
+            reward = 1
+
+        if self.attempts == 3:
+            self.attempts = 0
+            self.task = random.choice(self.tasks)
+
+        return self.task, reward
 
     def reset(self, symbol=None):
-        return self._next_observation()
-
-    def _next_observation(self):
-        ...
+        self.task = random.choice(self.tasks)
+        self.attempts = 0
+        return self.task
 
     def render(self, mode='human'):
-        ...
+        plot_task(self.task)
 
     def close(self):
         ...
@@ -28,7 +51,8 @@ class ARCEnv(gym.Env):
 
 if __name__ == '__main__':
     env = ARCEnv()
-    env.reset()
-    done = False
-    while not done:
-        obs, reward, done = env.step(env.action_space.sample())
+    obs = env.reset()
+
+    # cheat and give the correct answer
+    obs, reward = env.step(obs['test'][0]['output'])
+    print(reward)
